c3faf62444c9716e79fe1c9429c8db23eec02560
uo-puddles <59064719+uo-puddles@users.noreply.github.com>
2021-09-28 16:38:00 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,249 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import KNNImputer
+from sklearn.preprocessing import QuantileTransformer
+
+from sklearn.pipeline import Pipeline
+import pandas as pd
+import numpy as np
+
+#drop by removing or keeping
+class DropColumnsTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, column_list, action='drop'):
+    assert action in ['keep', 'drop'], f'DropColumnsTransformer action {action} not in ["keep", "drop"]'
+    self.column_list = column_list
+    self.action = action
+    
+  #fill in rest below
+  def fit(self, X, y = None):
+    print("Warning: DropColumnsTransformer.fit does nothing.")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'DropColumnsTransformer.transform expected Dataframe but got {type(X)} instead.'
+    
+    X_ = X.copy()
+    xcols = X.columns.to_list()
+    actual_list = [col for col in self.column_list if col in xcols]  #some might be missing from X columns
+    if self.action=='drop':
+      if actual_list != self.column_list:
+        print(f'DropColumnsTransformer.transform warning: columns to drop not in X: {set(self.column_list) - set(xcols)}')
+      X_ = X_.drop(columns=actual_list)
+    else:
+      if actual_list != self.column_list:
+        print(f'DropColumnsTransformer.transform warning: columns to keep not in X: {set(self.column_list) - set(xcols)}')
+      X_ = X_[actual_list]
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+  
+#This class maps values in a column, numeric or categorical.
+#Importantly, it does not change NaNs, leaving that for the imputer step.
+class MappingTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, mapping_column, mapping_dict:dict):  
+    self.mapping_dict = mapping_dict
+    self.mapping_column = mapping_column  #column to focus on
+
+  def fit(self, X, y = None):
+    print("Warning: MappingTransformer.fit does nothing.")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'MappingTransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert self.mapping_column in X.columns.to_list(), f'MappingTransformer.transform unknown column {self.mapping_column}'
+    X_ = X.copy()
+    X_[self.mapping_column].replace(self.mapping_dict, inplace=True)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+    
+
+class OHETransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, dummy_na=False, drop_first=True):  
+    self.target_column = target_column
+    self.dummy_na = dummy_na
+    self.drop_first = drop_first
+ 
+  def fit(self, X, y = None):
+    print("Warning: OHETransformer.fit does nothing.")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'OHETransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert self.target_column in X.columns.to_list(), f'OHETransformer.transform unknown column {self.target_column}'
+    X_ = X.copy()
+    X_ = pd.get_dummies(X_, columns=[self.target_column],
+                        dummy_na=self.dummy_na,
+                        drop_first = self.drop_first)
+    print(f'OHETransformer columns: {X_.columns.to_list()}')
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+  
+class Sigma3Transformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column):  
+    self.target_column = target_column
+    
+  def fit(self, X, y = None):
+    print("Warning: Sigma3Transformer.fit does nothing.")
+    return self
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'Sigma3Transformer.transform expected Dataframe but got {type(X)} instead.'
+    X_ = X.copy()
+    mean = X_[self.target_column].mean()
+    sigma = X_[self.target_column].std()
+    high_wall = mean + 3*sigma
+    low_wall = mean - 3*sigma
+    print(f'Sigma3Transformer mean, sigma, low_wall, high_wall: {round(mean, 2)}, {round(sigma, 2)}, {round(low_wall, 2)}, {round(high_wall, 2)}')
+    X_[self.target_column] = X_[self.target_column].clip(lower=low_wall, upper=high_wall)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+
+  
+class TukeyTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, fence='outer'):
+    assert fence in ['inner', 'outer']
+    self.target_column = target_column
+    self.fence = fence
+    
+  def fit(self, X, y = None):
+    print("Warning: Sigma3Transformer.fit does nothing.")
+    return self
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'Sigma3Transformer.transform expected Dataframe but got {type(X)} instead.'
+    X_ = X.copy()
+    q1 = X_[self.target_column].quantile(0.25)
+    q3 = X_[self.target_column].quantile(0.75)
+    iqr = q3-q1
+    inner_low = q1-1.5*iqr
+    outer_low = q1-3*iqr
+    inner_high = q1+1.5*iqr
+    outer_high = q3+3*iqr
+    print(f'TukeyTransformer inner_low, inner_high, outer_low, outer_high: {round(inner_low, 2)}, {round(outer_low, 2)}, {round(inner_high, 2)}, {round(outer_high, 2)}')
+    if self.fence=='inner':
+      X_[self.target_column] = X_[self.target_column].clip(lower=inner_low, upper=inner_high)
+    elif self.fence=='outer':
+      X_[self.target_column] = X_[self.target_column].clip(lower=outer_low, upper=outer_high)
+    else:
+      assert False, f"fence has unrecognized value {self.fence}"
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result  
+
+class MinMaxTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self):
+    self.column_stats = dict()
+
+  #fill in rest below
+  def fit(self, X, y = None):
+    assert isinstance(X, pd.core.frame.DataFrame), f'MinMaxTransformer.fit expected Dataframe but got {type(X)} instead.'
+    if y: print(f'Warning: MinMaxTransformer.fit did not expect a value for y but got {type(y)} instead.')
+    self.column_stats = {c:(X[c].min(),X[c].max()) for c in X.columns.to_list()}
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'MinMaxTransformer.transform expected Dataframe but got {type(X)} instead.'
+    assert self.column_stats, f'MinMaxTransformer.transform expected fit method to be called prior.'
+    X_ = X.copy()
+    fit_columns = set(self.column_stats.keys())
+    transform_columns = set(X_.columns.to_list())
+    not_fit = transform_columns - fit_columns
+    not_transformed = fit_columns - transform_columns
+    if not_fit: print(f'Warning: MinMaxTransformer.transform has more columns than fit: {not_fit}.')
+    if not_transformed: print(f'Warning: MinMaxTransformer.transform has fewer columns than fit: {not_transformed}.')
+    for c in fit_columns:
+      if c not in transform_columns: continue
+      cmin,cmax = self.column_stats[c]
+      denom = cmax-cmin
+      new_col = [(v-cmin)/denom for v in X_[c].to_list()]
+      X_[c] = new_col
+    return X_
+
+  def fit_transform(self, X, y = None):
+    self.fit(X,y)
+    result = self.transform(X)
+    return result
+  
+class KNNTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self,n_neighbors=5, weights="uniform", add_indicator=False):
+    self.n_neighbors = n_neighbors
+    self.weights=weights 
+    self.add_indicator=add_indicator
+
+  #your code
+  def fit(self, X, y = None):
+    print(f'Warning: KNNTransformer.fit does nothing.')
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'KNNTransformer.transform expected Dataframe but got {type(X)} instead.'
+    X_ = X.copy()
+    imputer = KNNImputer(n_neighbors=self.n_neighbors, weights=self.weights, add_indicator=self.add_indicator)
+    columns = X_.columns
+    matrix = imputer.fit_transform(X_)
+    result_df = pd.DataFrame(matrix,columns=columns)
+    return result_df
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+  
+class IterativeTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, estimator, max_iter=10, random_state=1234):
+    self.estimator = estimator
+    self.max_iter=max_iter 
+    self.random_state=random_state
+
+  #your code
+  def fit(self, X, y = None):
+    print(f'Warning: IterativeTransformer.fit does nothing.')
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'IterativeTransformer.transform expected Dataframe but got {type(X)} instead.'
+    X_ = X.copy()
+    imputer = IterativeImputer(estimator=self.estimator, max_iter=self.max_iter, random_state=self.random_state)
+    columns = X_.columns
+    matrix = imputer.fit_transform(X_)
+    result_df = pd.DataFrame(matrix,columns=columns)
+    return result_df
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+  
+#from week 2
+titanic_transformer_v1 = Pipeline(steps=[
+    ('gender', MappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('class', MappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
+    ('ohe', OHETransformer(target_column='Joined')),
+    ], verbose=True)
+
+#week 9
+
+def create_models(ensemble_df):
+  import json
+  model_list = []
+  for i in range(len(ensemble_df)):
+    row = ensemble_df.loc[i].to_list()
+    exp = f'{row[0]}('
+    #writing and reading to file stringifies the dictionary - have to change back
+    hps = json.loads(row[1]) if isinstance(row[1], str) else row[1]
+    for h in hps.keys():
+      exp += f'{h}={hps[h]},'
+    exp += ')'
+    print(exp)
+    model_list.append(eval(exp))
+  return model_list
