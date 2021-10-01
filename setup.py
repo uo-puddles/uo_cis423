7d47f45b8ccd840b87140765be99ab8896b3e854
uo-puddles <59064719+uo-puddles@users.noreply.github.com>
2021-10-01 14:53:45 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -92,3 +92,60 @@ class OHETransformer(BaseEstimator, TransformerMixin):
     result = self.transform(X)
     return result
   
+#chapter 4 asks for 2 new transformers
+
+class Sigma3Transformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column):  
+    self.target_column = target_column
+    
+  def fit(self, X, y = None):
+    print("Warning: Sigma3Transformer.fit does nothing.")
+    return self
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'Sigma3Transformer.transform expected Dataframe but got {type(X)} instead.'
+    X_ = X.copy()
+    mean = X_[self.target_column].mean()
+    sigma = X_[self.target_column].std()
+    high_wall = mean + 3*sigma
+    low_wall = mean - 3*sigma
+    print(f'Sigma3Transformer mean, sigma, low_wall, high_wall: {round(mean, 2)}, {round(sigma, 2)}, {round(low_wall, 2)}, {round(high_wall, 2)}')
+    X_[self.target_column] = X_[self.target_column].clip(lower=low_wall, upper=high_wall)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+
+class TukeyTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, fence='outer'):
+    assert fence in ['inner', 'outer']
+    self.target_column = target_column
+    self.fence = fence
+    
+  def fit(self, X, y = None):
+    print("Warning: Sigma3Transformer.fit does nothing.")
+    return self
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'Sigma3Transformer.transform expected Dataframe but got {type(X)} instead.'
+    X_ = X.copy()
+    q1 = X_[self.target_column].quantile(0.25)
+    q3 = X_[self.target_column].quantile(0.75)
+    iqr = q3-q1
+    inner_low = q1-1.5*iqr
+    outer_low = q1-3*iqr
+    inner_high = q1+1.5*iqr
+    outer_high = q3+3*iqr
+    print(f'TukeyTransformer inner_low, inner_high, outer_low, outer_high: {round(inner_low, 2)}, {round(outer_low, 2)}, {round(inner_high, 2)}, {round(outer_high, 2)}')
+    if self.fence=='inner':
+      X_[self.target_column] = X_[self.target_column].clip(lower=inner_low, upper=inner_high)
+    elif self.fence=='outer':
+      X_[self.target_column] = X_[self.target_column].clip(lower=outer_low, upper=outer_high)
+    else:
+      assert False, f"fence has unrecognized value {self.fence}"
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
